8c68625be498d72f5326829c87a3e2123570ab5f
Gustavo Guichard <gustavoguichard@gmail.com>
2015-09-26 22:08:49 -0300

    Fix bug in propTypes validation

--- src/propTypes.js
+++ src/propTypes.js
@@ -1,17 +1,19 @@
-const inRange = (val, min, max) => (val >= min && val <= max)
+const inRange = (val, range) => (val >= range[0] && val <= range[1])
 
 export const anglePropType = (props, propName, component) => {
-  const prop = props[propName], min = 0, max = 360
-  if (!inRange(prop, min, max) || (prop !== "vertical" && prop !== "horizontal")) {
+  const prop = props[propName],
+        range = [0, 360]
+  if (!inRange(prop, range) && (prop !== "vertical" && prop !== "horizontal")) {
     return new Error(`Invalid angle in propType: ${propName}.
-                      The angle must be horizontal, vertical or within ${min} and ${max}`)
+                      The angle must be horizontal, vertical or within ${range[0]} and ${range[1]}`)
   }
 }
 
 export const rangePropType = (props, propName, component) => {
-  const prop = props[propName], min = 0, max = 100
-  if (isNaN(prop) || !inRange(prop, min, max)) {
+  const prop = props[propName],
+        range = [0, 100]
+  if (isNaN(prop) || !inRange(prop, range)) {
     return new Error(`Invalid number in propType: ${propName}.
-                      The number must be within ${min} and ${max}`)
+                      The number must be within ${range[0]} and ${range[1]}`)
   }
 }
